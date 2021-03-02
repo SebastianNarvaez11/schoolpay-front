186fd89da61348693e85b03c05c43d6c4e3a68bb
SebastianNarvaez11 <narvaez.jhoan@correounivalle.edu.co>
2021-03-02 10:36:11 -0500

    add messge

--- src/helpers/messages.js
+++ src/helpers/messages.js
@@ -5,11 +5,11 @@ export const email_recordatorio = (user) => {
 }
 
 export const sms_recordatorio = (user) => {
-    return `COLEGIO ACADÉMICO EL POBLADO: Cordial saludo. Se informa que a la fecha, el estudiante ${user.first_name} ${user.last_name}, de grado ${user.student.grade.name}, adeuda un valor total de $ ${formatNumber(user.student.amountOwed)}, por concepto de ${user.student.monthOwed} mensualidades atrasadas. SMS DE PRUEBA`
+    return `COLEGIO ACADÉMICO EL POBLADO \n  \n*Mensaje Recordatorio* \n \nSeñor padre de familia cordial saludo, recuerde que el pago de la mensualidad se debe realizar los 5 primero días de cada mes; esto con el fin, de dar cumplimiento a las obligaciones administrativas y  financieras de la institución. \n \n*Si ya canceló agradecemos su pronto pago y compromiso.* \n \nAtte Departamento Financiero`
 }
 
 export const wpp_recordatorio = (user) => {
-    return `COLEGIO ACADÉMICO EL POBLADO: Cordial saludo. Se informa que a la fecha, el estudiante ${user.first_name} ${user.last_name} (Código: ${user.student.code}), de grado ${user.student.grade.name}, adeuda un valor total de $ ${formatNumber(user.student.amountOwed)}, por concepto de ${user.student.monthOwed} mensualidad(es) atrasadas. Se le recuerda al acudiente que los pagos deben realizarse los primeros 5 días del mes y se debe enviar la foto del comprobante de pago a este WhatsApp para su registro. Recomendamos efectuar el pago del valor adeudado lo más pronto posible. Agradecemos su atención y quedamos atentos a una pronta respuesta.`
+    return `COLEGIO ACADÉMICO EL POBLADO \n  \n*Mensaje Recordatorio* \n \nSeñor padre de familia cordial saludo, recuerde que el pago de la mensualidad se debe realizar los 5 primero días de cada mes; esto con el fin, de dar cumplimiento a las obligaciones administrativas y  financieras de la institución. \n \n*Si ya canceló agradecemos su pronto pago y compromiso.* \n \nAtte Departamento Financiero`
 }
 
 export const wpp_recordatorio2 = (user) => {
