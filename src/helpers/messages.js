9524c543c19c5a747546ac7485cadeabb2bd4fa6
SebastianNarvaez11 <narvaez.jhoan@correounivalle.edu.co>
2021-03-15 06:43:14 -0500

    refactor sms

--- src/helpers/messages.js
+++ src/helpers/messages.js
@@ -1,17 +1,17 @@
 import { formatNumber } from './functions'
 
 export const email_recordatorio = (user) => {
-    return `Se informa que a la fecha, el estudiante ${user.first_name} ${user.last_name}, de grado ${user.student.grade.name}, adeuda un valor total de $ ${formatNumber(user.student.amountOwed)}, por concepto de ${user.student.monthOwed} mensualidades atrasadas. Se le recuerda al acudiente que los pagos deben realizarse los primeros 5 dias del mes. Recomendamos efectuar el pago del valor adeudado lo mas pronto posible o dirigirse al departamento financiero del colegio para acordar un compromiso de pago.`
+    return `Se informa que a la fecha, el estudiante ${user.first_name} ${user.last_name}, de grado ${user.student.grade.name}, adeuda un valor total de $ ${formatNumber(user.student.amountOwed)}, por concepto de ${user.student.monthOwed} mensualidades atrasadas. Se le recuerda al acudiente que los pagos deben realizarse los primeros 10 dias del mes. Recomendamos efectuar el pago del valor adeudado lo mas pronto posible o dirigirse al departamento financiero del colegio para acordar un compromiso de pago.`
 }
 
 export const sms_recordatorio = (user) => {
-    return `COLEGIO ACADÉMICO EL POBLADO \n  \n*Mensaje Recordatorio* \n \nSeñor padre de familia cordial saludo, recuerde que el pago de la mensualidad se debe realizar los 5 primero días de cada mes; esto con el fin, de dar cumplimiento a las obligaciones administrativas y  financieras de la institución. \n \n*Si ya canceló agradecemos su pronto pago y compromiso.* \n \nAtte Departamento Financiero`
+    return `COLEGIO ACADÉMICO EL POBLADO \n  \n*Mensaje Recordatorio* \n \nSeñor padre de familia cordial saludo, recuerde que el pago de la mensualidad se debe realizar los 10 primeros días de cada mes; esto con el fin, de dar cumplimiento a las obligaciones administrativas y  financieras de la institución. \n \n*Si ya canceló agradecemos su pronto pago y compromiso.* \n \nAtte Departamento Financiero`
 }
 
 export const wpp_recordatorio = (user) => {
-    return `COLEGIO ACADÉMICO EL POBLADO \n  \n*Mensaje Recordatorio* \n \nSeñor padre de familia cordial saludo, recuerde que el pago de la mensualidad se debe realizar los 5 primero días de cada mes; esto con el fin, de dar cumplimiento a las obligaciones administrativas y  financieras de la institución. \n \n*Si ya canceló agradecemos su pronto pago y compromiso.* \n \nAtte Departamento Financiero`
+    return `COLEGIO ACADÉMICO EL POBLADO \n  \n*Mensaje Recordatorio* \n \nSeñor padre de familia cordial saludo, recuerde que el pago de la mensualidad se debe realizar los 10 primeros días de cada mes; esto con el fin, de dar cumplimiento a las obligaciones administrativas y  financieras de la institución. \n \n*Si ya canceló agradecemos su pronto pago y compromiso.* \n \nAtte Departamento Financiero`
 }
 
 export const wpp_recordatorio2 = (user) => {
-    return `COLEGIO ACADÉMICO EL POBLADO - SEGUNDO LLAMADO: Cordial saludo. Se informa que a la fecha, el estudiante ${user.first_name} ${user.last_name} (Código: ${user.student.code}), de grado ${user.student.grade.name}, adeuda un valor total de $ ${formatNumber(user.student.amountOwed)}, por concepto de ${user.student.monthOwed} mensualidad(es) atrasada(s). Se le recuerda al acudiente que los pagos deben realizarse los primeros 5 días del mes y se debe enviar la foto del comprobante de pago a este WhatsApp para su registro. Recomendamos efectuar el pago del valor adeudado lo más pronto posible. EVITE RECARGOS POR MORA.  Agradecemos su atención y quedamos atentos a una pronta respuesta.`
+    return `COLEGIO ACADÉMICO EL POBLADO: Cordial saludo. Se informa que a la fecha, el estudiante ${user.first_name} ${user.last_name} (Código: ${user.student.code}), de grado ${user.student.grade.name}, adeuda un valor total de $ ${formatNumber(user.student.amountOwed)}, por concepto de ${user.student.monthOwed} mensualidad(es) atrasada(s). Se le recuerda al acudiente que los pagos deben realizarse los primeros 10 días del mes y se debe enviar la foto del comprobante de pago a este WhatsApp para su registro. Recomendamos efectuar el pago del valor adeudado lo más pronto posible. Agradecemos su atención y quedamos atentos a una pronta respuesta.`
 }
